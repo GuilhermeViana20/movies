877bffbf15f0110f8086f169c12f8d0b8d60648a
Guilherme Viana <guilhermeviana347@gmail.com>
2023-07-08 13:57:27 -0300

    wip

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <HelloWorld />
+
+  <v-bottom-navigation grow>
+    <v-btn value="recent">
+      <v-icon>mdi-home</v-icon>
+      Home
+    </v-btn>
+
+    <v-btn value="favorites">
+      <v-icon>mdi-heart</v-icon>
+      Favoritos
+    </v-btn>
+
+    <v-btn value="nearby">
+      <v-icon>mdi-account</v-icon>
+
+      Conta
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+<script setup>
+  import HelloWorld from '@/components/HelloWorld.vue'
+</script>
